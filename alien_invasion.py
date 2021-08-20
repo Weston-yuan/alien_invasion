8c759cab0ee0097c7e66875fbca89f41920b2301
Weston-yuan <13697341844@163.com>
2021-08-20 10:59:18 +0800

    12.8完成

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,8 +11,15 @@ class AlienInvasion:
     pygame.init()
     self.settings = Settings()
 
+    #将游戏窗口尺寸设为固定值
     self.screen = pygame.display.set_mode(
       (self.settings.screen_width, self.settings.screen_height))
+
+    #将游戏窗口设为全屏
+    #self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    #self.settings.screen_width = self.screen.get_rect().width
+    #self.settings.screen_height = self.screen.get_rect().height
+
     pygame.display.set_caption("Alien Invasion")
 
     self.ship = Ship(self)
@@ -30,7 +37,6 @@ class AlienInvasion:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
-      #向右移动飞船
       elif event.type == pygame.KEYDOWN:
         self._check_keydown_events(event)
       elif event.type == pygame.KEYUP:
@@ -42,6 +48,8 @@ class AlienInvasion:
       self.ship.moving_right = True
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = True
+    elif event.key == pygame.K_q:
+      sys.exit()
 
   def _check_keyup_events(self, event):
     """响应松开"""
