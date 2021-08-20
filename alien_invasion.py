fa792ecd2be6e3c6fecbc6973336f6f7355d1c4a
Weston-yuan <13697341844@163.com>
2021-08-20 11:24:04 +0800

    实现上下左右方向移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,6 +48,10 @@ class AlienInvasion:
       self.ship.moving_right = True
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = True
+    elif event.key == pygame.K_UP:
+      self.ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+      self.ship.moving_down = True
     elif event.key == pygame.K_q:
       sys.exit()
 
@@ -57,6 +61,10 @@ class AlienInvasion:
       self.ship.moving_right = False
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = False
+    elif event.key == pygame.K_UP:
+      self.ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+      self.ship.moving_down = False
 
   def _update_screen(self):
     """更新屏幕图像，并切换到新屏幕"""
