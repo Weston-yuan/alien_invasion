39908ef5c54283579d0ae7d966f9268d8f548180
Weston-yuan <13697341844@163.com>
2021-08-20 09:46:53 +0800

    飞船的优化

--- settings.py
+++ settings.py
@@ -4,5 +4,8 @@ class Settings:
       """初始化游戏的设置"""
       #屏幕设置
       self.screen_width = 1200
-      self.screen_height = 800
-      self.bg_color = (230,230,230)
+      self.screen_height = 750
+      self.bg_color = (0,170,201)
+
+      #飞船设置
+      self.ship_speed = 1.5
