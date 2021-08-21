721e3318ae38f1ea7415cde15c03ad09199a9f38
Weston-yuan <13697341844@163.com>
2021-08-21 09:39:51 +0800

    完成13-5

--- settings.py
+++ settings.py
@@ -9,10 +9,17 @@ class Settings:
 
       #飞船设置
       self.ship_speed = 1.5
+      self.ship_limit = 3
 
       #子弹设置
-      self.bullet_speed = 1.0
+      self.bullet_speed = 1.5
       self.bullet_width = 3
       self.bullet_height = 15
       self.bullet_color = (255, 255, 255)
       self.bullets_allowed = 20
+
+      #外星人设置
+      self.alien_speed = 1.0
+      self.fleet_drop_speed = 20
+      #1表示向右，-1表示向左
+      self.fleet_direction = 1
